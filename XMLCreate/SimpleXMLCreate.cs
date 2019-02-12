39b8fa04a4b05941d6528a411c10e5649e1d48de
pablopsp <pablosilio@gmail.com>
2019-02-12 22:22:27 +0100

    Simple XML Create
    Create a simple XML passing it values

---
+++ XMLCreate/SimpleXMLCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XMLCreate
+{
+    public class SimpleXMLCreate
+    {
+        /// <summary>
+        /// Create a simple XML. Tuple is a Node and his Childs, 
+        /// and the dict inside the tuple is the Childs key and value
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="nodesANDValues"></param>
+        /// <param name="path"></param>
+        public static void CreateSimpleXML(string root, List<Tuple<string, IDictionary<string, string>>> nodesANDValues, string path)
+        {
+            XElement xRoot = new XElement(root);
+
+            foreach (Tuple<string, IDictionary<string, string>> tuple in nodesANDValues)
+            {
+                XElement apoyo = new XElement(tuple.Item1);
+                foreach (KeyValuePair<string, string> entry in tuple.Item2)
+                {
+                    apoyo.Add(new XElement(entry.Key, entry.Value));
+                }
+                xRoot.Add(apoyo);
+            }
+            new XDocument(xRoot).Save(path);
+        }
+    }
+}
